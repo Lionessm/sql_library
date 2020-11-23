d9710539f2316ebfa245e1085f04dc1809930919
Maria <maria.mihaila.17@gmail.com>
2020-11-24 00:10:48 +0200

    Establish view page-not-found in jade

--- app.js
+++ app.js
@@ -43,15 +43,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// 404 handling middleware
-app.use(function(req, res, next){
-    let error = new Error();
-    error.status = 404;
-    error.message = 'Sorry, the page could not be found!';
-    res.render('page-not-found', {error});
-    res.status(404);
-});
-
 // error handler
 app.use(function(err, req, res, next) {
 
@@ -63,17 +54,27 @@ app.use(function(err, req, res, next) {
         err.message = 'Sorry, an internal server error was detected';
     }
 
-  // set locals, only providing error in development
+    // set locals, only providing error in development
     res.locals.title = 'Error';
     res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  console.log('err.status ' , err.status);
-  console.log('err.message ' , err.message);
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    console.log('err.status ' , err.status);
+    console.log('err.message ' , err.message);
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
+// 404 handling middleware
+app.use(function(req, res, next){
+    res.locals.title = 'Page not found';
+    let error = new Error();
+    error.status = 404;
+    error.message = 'Sorry, the page could not be found!';
+
+    res.status(404);
+    res.render('page-not-found', {error});
+});
 
 module.exports = app;
