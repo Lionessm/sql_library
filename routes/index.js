9642f2c81421f450d3d359391052f811aa11c8b2
Maria <maria.mihaila.17@gmail.com>
2020-11-23 18:11:32 +0200

    Test the Book model and communication with the database. Import Book model from the ../models folder. In the body of the GET '/' route handler provided for you.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { Book } = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+
+  //res.render('index', { title: 'Express' });
+  const books = await Book.findAll({})
+  return res.json(books);
 });
 
 module.exports = router;
