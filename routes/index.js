1e88cc5f07335055c134631f3891c596624349fb
Maria <maria.mihaila.17@gmail.com>
2020-11-24 15:33:56 +0200

    Create routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { Book } = require('../models');
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
+// Handler function to wrap each route. //
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
-  //res.render('index', { title: 'Express' });
-  const books = await Book.findAll({})
-  return res.json(books);
+/* GET home page. */
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render('all_books', {books});
+}));
+
+// ROUTES
+router.get('/', function (req, res) {
+  res.redirect('/books')
+});
+
+// Shows the create new book form
+router.get('/books/new ', function(req, res) {
+  res.render('new_book')
+});
+// Posts a new book to the database -----?????HOW
+router.post('/books/new ', function(req, res) {
+});
+//  Shows book detail form
+router.get('/books/:id', function (req, res) {
+  res.render('book_detail')
+});
+// Updates book info in the database -----??????
+router.post('/books/:id', function(req,res) {
+
+});
+// Deletes a book
+router.post('/books/:id/delete', function(req,res) {
+
 });
 
 module.exports = router;
