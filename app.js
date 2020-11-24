1c9b7b293778c5a8faefb0b9bdb90e8b53fcf0d2
Maria <maria.mihaila.17@gmail.com>
2020-11-24 15:34:24 +0200

    edit views pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const pug = require('pug');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const {Sequelize} = require('sequelize');
@@ -32,7 +33,7 @@ app.use(express.static('public'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -44,57 +45,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-// Handler function to wrap each route. //
-function asyncHandler(cb){
-    return async(req, res, next) => {
-        try {
-            await cb(req, res, next)
-        } catch(error){
-            // Forward error to the global error handler
-            next(error);
-        }
-    }
-}
-
-// ROUTES
-app.get('/', asyncHandler(async (req, res) => {
-    const books = await Book.findAll();
-    res.render("/books", { books, title: "Books" });
-}));
-
-// Shows the full list of books ------ !!!! CREATE VIEW FOR ALL-BOOKS
-app.get('/books', function (req, res) {
-    res.render('all_books');
-});
-// Shows the create new book form
-app.get('/books/new ', function(req, res) {
-    res.render('new_book')
-});
-// Posts a new book to the database -----?????HOW
-app.post('/books/new ', function(req, res) {
-});
-//  Shows book detail form
-app.get('/books/:id', function (req, res) {
-    res.render('book_detail')
-});
-// Updates book info in the database -----??????
-app.post('/books/:id', function(req,res) {
-
-});
-// Deletes a book
-app.post('/books/:id/delete', function(req,res) {
-
-});
-
 
 // error handler
 app.use(function(err, req, res, next) {
-
     if (!err.status) {
         err.status = 500;
     }
 
-    if (err) {
+    if (!err.message) {
         err.message = 'Sorry, an internal server error was detected';
     }
 
