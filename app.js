b664e646093fbe0b5605a063df492e8a800bbfed
Maria <maria.mihaila.17@gmail.com>
2020-11-24 14:04:48 +0200

    Create routes

--- app.js
+++ app.js
@@ -43,6 +43,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+// Handler function to wrap each route. //
+function asyncHandler(cb){
+    return async(req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch(error){
+            // Forward error to the global error handler
+            next(error);
+        }
+    }
+}
+
+// ROUTES
+app.get('/', asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    res.render("/books", { books, title: "Books" });
+}));
+
+// Shows the full list of books ------ !!!! CREATE VIEW FOR ALL-BOOKS
+app.get('/books', function (req, res) {
+    res.render('all_books');
+});
+// Shows the create new book form
+app.get('/books/new ', function(req, res) {
+    res.render('new_book')
+});
+// Posts a new book to the database -----?????HOW
+app.post('/books/new ', function(req, res) {
+});
+//  Shows book detail form
+app.get('/books/:id', function (req, res) {
+    res.render('book_detail')
+});
+// Updates book info in the database -----??????
+app.post('/books/:id', function(req,res) {
+
+});
+// Deletes a book
+app.post('/books/:id/delete', function(req,res) {
+
+});
+
+
 // error handler
 app.use(function(err, req, res, next) {
 
